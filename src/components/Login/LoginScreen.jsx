bbfb413fe73101f2fb802ba697fbf8d9f6a1aaef
Santari Alejandro <alejandrosantari@gmail.com>
2021-01-08 13:52:23 -0300

    LOGIN

--- src/components/Login/LoginScreen.jsx
+++ src/components/Login/LoginScreen.jsx
@@ -1,6 +1,15 @@
 import React from "react"
 
+import { useForm } from "../../hooks/useForm"
+
 export const LoginScreen = () => {
+  const [formValues, errors, handleInputChange, , verification] = useForm()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    verification(e)
+  }
+
   return (
     <div className="login__main">
       <div className="login__box-content">
@@ -9,8 +18,9 @@ export const LoginScreen = () => {
         </div>
         <h2>Elecciones 2021</h2>
         <h3>Sistema de conteo</h3>
-        <form className="login__box-form">
+        <form className="login__box-form" onSubmit={handleSubmit}>
           <input
+            id="userLogin"
             type="text"
             placeholder="Usuario"
             className="login__box-form__userName"
@@ -18,16 +28,39 @@ export const LoginScreen = () => {
             name="userLogin"
             autoFocus="on"
             maxLength="50"
-            minLength="2"
+            onChange={handleInputChange}
           />
+          <div className="login__box-form__wrong-input">
+            <span
+              className={`errorInput ${
+                errors.userLogin === "noData" ? "visible animation" : "hidden"
+              }`}
+            >
+              Ingrese usuario
+            </span>
+          </div>
+
           <input
+            id="userPassword"
             className="login__box-form__userPassword"
             placeholder="Contraseña"
             name="userPassword"
             maxLength="50"
-            minLength="2"
             type="password"
+            onChange={handleInputChange}
           />
+          <div className="login__box-form__wrong-input">
+            <span
+              className={`errorInput ${
+                errors.userPassword === "noData"
+                  ? "visible animation"
+                  : "hidden"
+              }`}
+            >
+              Ingrese contraseña
+            </span>
+          </div>
+
           <button className="login__box-form__submit" type="submit">
             Ingresar
           </button>
---
+++ src/hooks/useForm.js
@@ -0,0 +1,60 @@
+import { useState } from "react"
+
+export const useForm = (initialState = {}) => {
+  const [values, setvalues] = useState(initialState)
+  const [errors, setErrors] = useState({
+    userLogin: "data",
+    userPassword: "data",
+  })
+
+  const reset = () => {
+    setvalues(initialState)
+  }
+
+  const handleInputChange = ({ target }) => {
+    const val = target.name
+    if (values.val?.class.length > 0) {
+      setvalues({
+        ...values,
+        [target.name]: {
+          [target.name]: target.value,
+        },
+      })
+    } else {
+      setvalues({
+        ...values,
+        [target.name]: {
+          value: target.value,
+          classNameOriginal: target.className,
+        },
+      })
+    }
+  }
+
+  const verification = ({ target }) => {
+    const user = target[0]
+    const pass = target[1]
+    let userStatus = ""
+    let passStatus = ""
+
+    if (user.value?.length < 1) {
+      userStatus = "noData"
+    } else {
+      userStatus = "data"
+    }
+
+    if (pass.value?.length < 1) {
+      passStatus = "noData"
+    } else {
+      passStatus = "data"
+    }
+
+    setErrors({
+      ...errors,
+      [user.id]: userStatus,
+      [pass.id]: passStatus,
+    })
+  }
+
+  return [values, errors, handleInputChange, reset, verification]
+}
