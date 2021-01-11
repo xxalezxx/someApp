45c1862aba255338456a645a62f9b64a76eb31b4
Santari Alejandro <alejandrosantari@gmail.com>
2021-01-11 19:56:26 -0300

    UPDATES TABLESFORM Se hace actualziacion de visual en barra de contenido d einformacion de mesas

--- src/components/Categories/CategoriesList.jsx
+++ src/components/Categories/CategoriesList.jsx
@@ -4,9 +4,14 @@ import { CategoryCard } from "./CategoryCard"
 export const CategoriesList = ({ list }) => {
   return (
     <div className="categoriesList__main">
-      {list.map((item) => (
-        <CategoryCard key={item.codigo} {...item} />
-      ))}
+      <div className="categoriesList__main-title">
+        Categorias actuales disponibles
+      </div>
+      <div className="categoriesList__main-content">
+        {list.map((item) => (
+          <CategoryCard key={item.codigo} {...item} />
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/Categories/CategoryCard.jsx
+++ src/components/Categories/CategoryCard.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
+import React from "react"
 
-export const CategoryCard = ({
-    id,
-    categoria,
-    codigo,
-    listas
-}) => {
-    return (
-        <div>
-            {categoria}
-            {codigo}
-        </div>
-    )
+export const CategoryCard = ({ id, categoria, codigo, listas }) => {
+  return (
+    <div className="categoryCard__card">
+      {categoria}
+      {codigo}
+    </div>
+  )
 }
